46137f48a0d51301a27a1309063d450df5b157d8
Ryan Huddleston <rhuddleston@Ryans-MacBook-Pro.local>
2018-09-13 16:29:41 -0600

    added some code so we see how we filter out passwords under a certain length to make it smaller if we don't allow passwords under that length

--- push.sh
+++ push.sh
@@ -1,2 +1,4 @@
+# awk 'length>9' build_collection/top1000000 > build_collection/top113362
+# node build_collection/index.js build_collection/top113362 collections/mostcommon_113362
 aws s3 cp ./build/nbp.min.js s3://static.quiq-cdn.com/assets/
 aws s3 cp collections/mostcommon_113362 s3://static.quiq-cdn.com/assets/
